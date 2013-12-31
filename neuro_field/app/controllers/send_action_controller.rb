26e7432037fe460bc563fa11aa71f5a4641493d8
Nikita <vampire@vampire-pc.Dlink>
2014-01-01 01:35:30 +0400

    almost works

--- neuro_field/app/controllers/send_action_controller.rb
+++ neuro_field/app/controllers/send_action_controller.rb
@@ -2,19 +2,20 @@ class SendActionController < ApplicationController
   def initialize *args
     super(&args)
   end
-  def turn id, action
+  def turn 
+    id, action = params[:id].to_i, params[:action] 
     unless $neuro_nets[id].performed_turn
       if action == 'eat'
-	$field[$neuro_nets[id].coor_x][ $neuro_nets[id].coor_y] = $neuro_nets[id].eat($field[$neuro_nets[id].coor_x][ $neuro_nets[id].coor_y])
+	$field[$neuro_nets[id].coor_x][ $neuro_nets[id].coor_y] = $neuro_nets[id].eat($field[$neuro_nets[id].coor_y][ $neuro_nets[id].coor_x])
       end
       if action == 'left'
 	$neuro_nets[id].coor_x = $neuro_nets[id].coor_x - 1 unless ($neuro_nets[id].coor_x - 1 < 0)   
       end
       if action == 'right'
-	$neuro_nets[id].coor_x = $neuro_nets[id].coor_x + 1 unless ($neuro_nets[id].coor_x + 1 > $field[0].length)
+	$neuro_nets[id].coor_x = $neuro_nets[id].coor_x + 1 unless ($neuro_nets[id].coor_x + 1 > $field.length)
       end
       if action == 'up'
-	$neuro_nets[id].coor_y = $neuro_nets[id].coor_y + 1 unless ($neuro_nets[id].coor_y + 1 > $field.length)
+	$neuro_nets[id].coor_y = $neuro_nets[id].coor_y + 1 unless ($neuro_nets[id].coor_y + 1 > $field[0].length)
       end
       if action == 'down'
 	$neuro_nets[id].coor_y = $neuro_nets[id].coor_y - 1 unless ($neuro_nets[id].coor_y - 1 < 0)
